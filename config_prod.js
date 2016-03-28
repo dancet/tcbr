24bdc1bc52d705e3af3913dd60d207639b696e4b
Tom Dance <tom.dance@gmail.com>
2016-03-28 13:33:16 +1000

    Add prod config for scraper

--- config_prod.js
+++ config_prod.js
@@ -2,13 +2,13 @@ var config = module.exports = require('./config_defaults.js');
 
 config.fundraising_file = '/home/ec2-user/tcbr/fundraising-total.txt'
 
-// config.logger.scraper = {
-// 	name: "pixo-scraper",
-//     streams: [
-// 	    {
-// 	        type: 'rotating-file',
-// 	        path: 'scraper.log',
-// 	        period: '1d'
-// 	    }
-//     ]	
-// };
+config.logger.scraper = {
+	name: "pixo-scraper",
+    streams: [
+	    {
+	        type: 'rotating-file',
+	        path: '/home/ec2-user/tcbr/scraper.log',
+	        period: '1d'
+	    }
+    ]	
+};
